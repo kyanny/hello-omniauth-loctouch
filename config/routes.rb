64ec97632a78bbf40b577ed8e90a4929f8776652
Kensuke Nagae <kyanny@gmail.com>
2012-01-23 22:23:32 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ HelloLoctouch::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  
+  match '/auth/:provider/callback' => 'sessions#callback'
+  match '/signout' => 'sessions#destroy', :as => :signout
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
