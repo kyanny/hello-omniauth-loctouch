baa56257c96994c8753282a8f478b1557190d803
Kensuke Nagae <kyanny@gmail.com>
2012-01-23 22:08:13 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloLoctouch::Application.config.secret_token = '05e91a997bace7779d2099c06063038e3fa64688aee8e8882e6e99beded121ec19b07cff35ac5bad84aa917ca74960b957c6e285d805b9a5a6bb369dfb059c8f'
