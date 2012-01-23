fc0da29d305a26d7c6e14468b6ed46ed80ddfef4
Kensuke Nagae <kyanny@gmail.com>
2012-01-23 22:30:19 +0900

    rails g controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HelloLoctouch::Application.routes.draw do
+  get "test/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -51,7 +53,7 @@ HelloLoctouch::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'test#index'
 
   # See how all your routes lay out with "rake routes"
 
@@ -59,3 +61,6 @@ HelloLoctouch::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 end
+
+
+
